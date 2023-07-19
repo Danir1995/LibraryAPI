daadc99952b99a81c47e61fd6dec4f9066aa11b8
DanirPT <danir.hucaev@gmail.com>
2023-07-19 01:42:48 +0100

    added main class LibraryAPIApplication

---
+++ src/main/java/com/danir/libraryAPI/LibraryAPIApplication.java
@@ -0,0 +1,14 @@
+package com.danir.libraryAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAPIApplication.class, args);
+
+    }
+
+}
