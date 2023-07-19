1d299240e0c52871e5492a1781c0a5ac56bbbdc3
DanirPT <danir.hucaev@gmail.com>
2023-07-19 01:17:06 +0100

    added classes Person and Book using Lombok

---
+++ src/main/java/com/danir/libraryAPI/models/Book.java
@@ -0,0 +1,55 @@
+package com.danir.libraryAPI.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+
+@Entity
+@Table(name = "book")
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id", nullable = false)
+    private int bookId;
+
+    @Column
+    @NotNull(message = "book must has name!")
+    @Size(min = 2, max = 64,message = "name of book must be between 2 and 64 characters")
+    private String name;
+
+    @Column
+    @NotNull(message = "fill the author")
+    @Size(min = 2, max = 100, message = "name of author must be between 2 and 100 symbols")
+    private String author;
+
+    @Column
+    private int year;
+
+    @Column(name = "borrowed_date")
+    private OffsetDateTime borrowedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+    public Book() {
+    }
+
+    public Book(String name, int year, String author) {
+        this.author = author;
+        this.name = name;
+        this.year = year;
+    }
+
+    public boolean isOverdue() {
+        return borrowedDate != null && borrowedDate.isBefore(OffsetDateTime.now().minusDays(10));
+    }
+
+}
---
+++ src/main/java/com/danir/libraryAPI/models/Person.java
@@ -0,0 +1,44 @@
+package com.danir.libraryAPI.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "person")
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id", nullable = false)
+    private int personId;
+
+    @Column(name = "full_name")
+    @Size(min = 2, max = 32,message = "name must be between 2 and 32 characters")
+    @NotNull(message = "full name can not be empty")
+    private String fullName;
+
+    @Column(name = "year_of_birth")
+    @Min(value = 1920, message = "Year can not be less than 1920")
+    @NotNull(message = "fill this line")
+    private int yearOfBirth;
+
+    @OneToMany(mappedBy = "person")
+    private List<Book> bookList;
+
+    public Person() {
+    }
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+}
