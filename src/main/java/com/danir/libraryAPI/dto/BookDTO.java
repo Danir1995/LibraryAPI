aaecc79b0c976c39c177d019fcc8c598e2aca816
DanirPT <danir.hucaev@gmail.com>
2023-07-20 02:01:07 +0100

    added classes BookDTO, PersonDTO

---
+++ src/main/java/com/danir/libraryAPI/dto/BookDTO.java
@@ -0,0 +1,21 @@
+package com.danir.libraryAPI.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookDTO {
+
+    @NotNull(message = "book must has name!")
+    @Size(min = 2, max = 64,message = "name of book must be between 2 and 64 characters")
+    private String name;
+
+    @NotNull(message = "fill the author")
+    @Size(min = 2, max = 100, message = "name of author must be between 2 and 100 symbols")
+    private String author;
+
+    private int year;
+}
---
+++ src/main/java/com/danir/libraryAPI/dto/PersonDTO.java
@@ -0,0 +1,20 @@
+package com.danir.libraryAPI.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonDTO {
+
+    @Size(min = 2, max = 32,message = "name must be between 2 and 32 characters")
+    @NotNull(message = "full name can not be empty")
+    private String fullName;
+
+    @Min(value = 1920, message = "Year can not be less than 1920")
+    @NotNull(message = "fill this line")
+    private int yearOfBirth;
+}
