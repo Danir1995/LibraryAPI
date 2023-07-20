149deae325598db60f965994ab66da3a49bc65e3
DanirPT <danir.hucaev@gmail.com>
2023-07-20 01:59:46 +0100

    added Bean of ModelMapper in LibraryAPIApplication class

--- src/main/java/com/danir/libraryAPI/LibraryAPIApplication.java
+++ src/main/java/com/danir/libraryAPI/LibraryAPIApplication.java
@@ -2,13 +2,18 @@ package com.danir.libraryAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
 public class LibraryAPIApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryAPIApplication.class, args);
-
     }
 
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
