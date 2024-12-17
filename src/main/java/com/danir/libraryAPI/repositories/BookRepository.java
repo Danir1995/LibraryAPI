bcd5581f05c7b18c1908e7bb1c2b57a11bda8245
DanirPT <danir.hucaev@gmail.com>
2024-12-17 20:54:44 +0000

    add new query to find overdue books to BookRepository.java

--- src/main/java/com/danir/libraryAPI/repositories/BookRepository.java
+++ src/main/java/com/danir/libraryAPI/repositories/BookRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Repository
@@ -17,4 +18,6 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
     @Query("from Book b where b.name like :title%")
     List<Book> findByTitleStartingWith(@Param("title") String title);
     Page<Book> findByPersonIsNullAndReservedByIsNull(Pageable pageable);
+    @Query("select b from Book b where b.borrowedDate is not null and b.borrowedDate <= :tenDaysAgo")
+    List<Book> findOverdueBooks(OffsetDateTime tenDaysAgo);
 }
