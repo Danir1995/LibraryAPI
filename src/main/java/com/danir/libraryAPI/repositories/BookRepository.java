3fee4cfd34e30118207d1ebbe2ad5df2b1501f77
DanirPT <danir.hucaev@gmail.com>
2024-12-19 02:37:24 +0000

    added updateOverdueBooks() in the BookRepository.java to update flag isOverdue

--- src/main/java/com/danir/libraryAPI/repositories/BookRepository.java
+++ src/main/java/com/danir/libraryAPI/repositories/BookRepository.java
@@ -1,9 +1,11 @@
 package com.danir.libraryAPI.repositories;
 
 import com.danir.libraryAPI.models.Book;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -19,5 +21,9 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
     List<Book> findByTitleStartingWith(@Param("title") String title);
     Page<Book> findByPersonIsNullAndReservedByIsNull(Pageable pageable);
     @Query("select b from Book b where b.borrowedDate is not null and b.borrowedDate <= :tenDaysAgo")
-    List<Book> findOverdueBooks(OffsetDateTime tenDaysAgo);
+    Page<Book> findOverdueBooks(OffsetDateTime tenDaysAgo, Pageable pageable);
+    @Modifying
+    @Transactional
+    @Query("UPDATE Book b SET b.isOverdue = TRUE WHERE b.borrowedDate <= CURRENT_DATE - 10")
+    void updateOverdueBooks();
 }
