3e2497d69ab4bbfcf5c977d60cb232697d250d82
DanirPT <danir.hucaev@gmail.com>
2025-02-04 20:32:05 +0000

    feat: add logging to BookService for better traceability -Integrated @Slf4j for logging

--- src/main/java/com/danir/libraryAPI/services/BookService.java
+++ src/main/java/com/danir/libraryAPI/services/BookService.java
@@ -42,35 +42,43 @@ public class BookService {
     }
 
     public List<Book> findAll() {
+        log.info("Fetching all books");
         return bookRepository.findAll();
     }
 
     public Book findOne(int id) {
+        log.info("Fetching book with ID: {}", id);
         return bookRepository.findById(id).orElse(null);
     }
 
     @Transactional
     public void save(Book book) {
+        log.info("Saving new book: {}", book.getName());
         bookRepository.save(book);
     }
 
     @Transactional
     public void update(int id, Book book) {
+        log.info("Updating book with ID: {}", id);
         Book existingBook = bookRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Book with id " + id + " not found"));
 
         existingBook.setName(book.getName());
         existingBook.setAuthor(book.getAuthor());
         existingBook.setYear(book.getYear());
+
+        bookRepository.save(existingBook);
     }
 
     @Transactional
     public void delete(long id) {
+        log.warn("Deleting book with ID: {}", id);
         bookRepository.deleteById((int) id);
     }
 
     @Transactional
     public void savePersonWithBook(Person person, Book book) {
+        log.info("Assigning book '{}' to person '{}'", book.getName(), person.getFullName());
         book.setPerson(person);
         book.setBorrowedDate(OffsetDateTime.now());
 
@@ -79,14 +87,19 @@ public class BookService {
 
     @Transactional
     public void release(Book book, int id) {
+        log.info("Releasing book with ID: {}", id);
         if (book == null) {
+            log.error("Attempted to release a null book");
             throw new IllegalArgumentException("Book cannot be null");
         }
 
         if (book.getBookId() != id) {
+            log.error("Mismatch between provided ID and book's ID");
             throw new IllegalArgumentException("The provided ID does not match the book's ID");
         }
+
         Person person = book.getPerson();
+
         if (person != null) {
             // add book like borrowed before
             BorrowedBook borrowedBook = new BorrowedBook();
@@ -99,6 +112,7 @@ public class BookService {
             // add book to user's borrowing history
             person.getBorrowedBeforeBooks().add(borrowedBook);
         }
+
         book.setPerson(null);
         book.setBorrowedDate(null);
         book.setOverdue(false);
@@ -113,15 +127,18 @@ public class BookService {
 
 
     public Page<Book> findAll(PageRequest pageRequest) {
+        log.info("Fetching all books from dataBase");
         return bookRepository.findAll(pageRequest);
     }
 
     public List<Book> searchBooksByTitle(String title) {
+        log.info("Searching book with title {}", title);
         return bookRepository.findByTitleStartingWith(title);
     }
 
     @Transactional
     public String reserveBook(int bookId, int personId) {
+        log.info("Reserving book ID {} for person ID {}", bookId, personId);
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new IllegalArgumentException("Book not found"));
 
@@ -134,29 +151,36 @@ public class BookService {
 
         book.setReservedBy(person);
         bookRepository.save(book);
+
         return null;
     }
 
     @Transactional
     public void cancelReservation(int bookId) {
+        log.info("Canceling reservation for book ID: {}", bookId);
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new IllegalArgumentException("Book not found"));
 
         book.setReservedBy(null);
+
         bookRepository.save(book);
     }
 
     public Page<Book> getAvailableBooks(PageRequest pageRequest) {
+        log.info("Fetching only available books");
         return bookRepository.findByPersonIsNullAndReservedByIsNull(pageRequest);
     }
 
     public BookDTO getBookDetails(int id) {
+        log.info("Getting details of the book with id: {}", id);
         Book book = findOne(id);
         BookDTO bookDTO = convertToBookDTO(book);
         bookDTO.setOverdue(isOverdue(book));
+
         if (book.getPerson() != null) {
             bookDTO.setPerson_name(book.getPerson().getFullName());
         }
+
         if (book.getReservedBy() != null) {
             bookDTO.setReserved_by_name(book.getReservedBy().getFullName());
         }
@@ -167,21 +191,25 @@ public class BookService {
     @Scheduled(cron = "0 1 0 * * ?")
     @Transactional
     public void updateOverdueBooks() {
+        log.info("Updating overdue books");
         bookRepository.updateOverdueBooks();
     }
 
     public boolean isOverdue(Book book) {
+        log.info("Checking if the book with id: {} is overdue and changing flag if needs", book.getBookId());
         OffsetDateTime date = book.getBorrowedDate();
         return date != null && date.isBefore(OffsetDateTime.now().minusDays(10));
     }
 
     @Transactional
     public double calculateDebt(Book book) {
+        log.info("Calculating debt for book: {}", book.getName());
         if (book.getIsDebtPaid() == null){
             return 0.0;
         }
+
         if (Boolean.TRUE.equals(book.getIsDebtPaid())) {
-            return 0.0; // Если долг уже оплачен, возвращаем 0
+            return 0.0;
         }
 
         OffsetDateTime borrowedDate = book.getBorrowedDate();
@@ -191,8 +219,8 @@ public class BookService {
         }
 
         long overdueDays = Duration.between(borrowedDate, OffsetDateTime.now()).toDays();
-        double amount;
 
+        double amount;
         amount = overdueDays > 10
                 ? 10 + (overdueDays - 10) * 5.0
                 : overdueDays * 1.0;
