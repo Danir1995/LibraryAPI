fa2c32867ef538a88cad5b20ed5e0ebd17e73097
DanirPT <danir.hucaev@gmail.com>
2025-02-07 14:38:08 +0000

    Added SLF4J logs for user-related operations
    - Added informational and warning messages for user creation, updates, and deletion.
    - Logs added for page accesses, successful and unsuccessful operations, and warnings (e.g., if email already exists or password is too short).
    - Logs will help track the sequence of events and identify potential issues in data handling.

--- src/main/java/com/danir/libraryAPI/controllers/PeopleController.java
+++ src/main/java/com/danir/libraryAPI/controllers/PeopleController.java
@@ -51,6 +51,8 @@ public class PeopleController {
 
         model.addAttribute("people", peopleService.findAll());
         model.addAttribute("currentUser", person.getPersonId());
+
+        log.info("Admin {} accessed the people index page.", userName);
         return "people/peopleIndex";
     }
 
@@ -69,12 +71,15 @@ public class PeopleController {
         model.addAttribute("reservedBookList", person.getReservedBooks());
         model.addAttribute("totalDebt", totalDebt);
         model.addAttribute("borrowedBeforeBooks", borrowedBooks);
+
+        log.info("Displaying details for person with ID: {}", id);
         return "people/show";
     }
 
     @GetMapping("/new")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String newPerson(@ModelAttribute("person") PersonDTO personDTO){
+        log.info("Admin is creating a new person.");
         return "people/new";
     }
 
@@ -83,21 +88,26 @@ public class PeopleController {
     public String create(@ModelAttribute("person") @Valid PersonDTO personDTO,
                          BindingResult result) {
         if (result.hasErrors()){
+            log.warn("Validation errors occurred while creating a new person.");
             return "people/new";
         }
 
         if (peopleService.emailExists(personDTO.getEmail())) {
             result.rejectValue("email", "error.person", "Email already exists");
+            log.warn("Email already exists.");
         }
 
         validator.validate(personDTO, result);
 
         if (personDTO.getPassword() == null || personDTO.getPassword().length() < 6){
             result.rejectValue("password", "error.person", "Password must be at least 6 characters long");
+            log.warn("Password for email is too short.");
             return "people/new";
         }
 
         peopleService.save(convertToPerson(personDTO));
+
+        log.info("New person created successfully.");
         return "redirect:/people";
     }
 
@@ -109,6 +119,8 @@ public class PeopleController {
 
         model.addAttribute("person", person);
         model.addAttribute("isSelf", isSelf);
+
+        log.info("Editing person details for ID: {}", id);
         return "people/personEdit";
     }
 
@@ -116,6 +128,7 @@ public class PeopleController {
     public String update(@ModelAttribute("person") @Valid PersonDTO personDTO, BindingResult result,
                          @PathVariable("id") int id){
         if (result.hasErrors()){
+            log.warn("Validation errors occurred while updating person with ID: {}", id);
             return "people/personEdit";
         }
 
@@ -123,6 +136,7 @@ public class PeopleController {
 
         if (peopleService.emailExists(personDTO.getEmail()) && !person.getEmail().equals(personDTO.getEmail())) {
             result.rejectValue("email", "error.person", "Email already exists");
+            log.warn("Email already exists!");
         }
 
         if (!personDTO.getFullName().equals(person.getFullName())){
@@ -136,10 +150,13 @@ public class PeopleController {
         ) {
 
             result.rejectValue("password", "error.person", "Password must be at least 6 characters long");
+            log.warn("Password is too short.");
             return "people/personEdit";
         }
 
         peopleService.update(id, convertToPerson(personDTO));
+
+        log.info("Updated person details for ID: {}", id);
         return "redirect:/people/" + id;
     }
 
@@ -150,9 +167,12 @@ public class PeopleController {
         // Check if person has assigned books
         if (!person.getBookList().isEmpty()) {
             redirectAttributes.addFlashAttribute("errorMessage", "Cannot delete user before receiving books");
+            log.warn("Cannot delete person with ID {} because they have assigned books.", id);
             return "redirect:/people/" + id;
         }
         peopleService.delete(id);
+
+        log.info("Deleted person with ID: {}", id);
         return "redirect:/people";
     }
 
